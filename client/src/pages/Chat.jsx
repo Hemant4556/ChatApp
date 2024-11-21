a33603316e2461b48700c32d4ac3353d116ab81a
Hemant4556 <Hemantkumar45506@gmail.com>
2024-11-21 13:32:43 +0530

    Hemant

--- client/src/pages/Chat.jsx
+++
@@ -1,5 +0,0 @@
-const Chat = () => {
-    return (<>chat App</> );
-};
- 
-export default Chat; 
--- server/Routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express =require("express");
-const {registerUser,loginuser,finduser,getUser} =require("../Controllers/userController");
-const router=express.Router();
-
-router.get("/register",registerUser);
-router.post("/login",loginuser);
-router.get("/find/:userId",finduser);
-router.get("/getuser",getUser);
-module.exports= router;
