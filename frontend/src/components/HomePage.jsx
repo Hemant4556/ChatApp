f8d33c594cf87865ed1c90d79873e08e0ae87d7e
Hemant4556 <Hemantkumar45506@gmail.com>
2024-11-21 13:25:43 +0530

    hemant

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import Sidebar from './Sidebar'
+import MessageContainer from './MessageContainer'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+const HomePage = () => {
+  const { authUser } = useSelector(store => store.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!authUser) {
+      navigate("/login");
+    }
+  }, []);
+  return (
+    <div className='flex md:h-[800px]  backdrop-blur-lg bg-opacity-0 w-[1100px] '>
+      <Sidebar />
+      <MessageContainer />
+    </div>
+  )
+}
+
+export default HomePage;
