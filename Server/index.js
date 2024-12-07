c8bdafe928d5263237b58c3643157736a7d26899
Hemant kumar <122089115+Hemant4556@users.noreply.github.com>
2024-12-07 14:31:18 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-    origin: 'https://chatapp-9bob.onrender.com/', // Adjust based on frontend URL
+    origin: 'https://chatapp-9bob.onrender.com', // Adjust based on frontend URL
     credentials: true,
 };
 app.use(cors(corsOption));
